b537f91f83312576e070c5247e784ecd35c7432b
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-23 07:22:59 +0900

    데이터베이스용 localforage

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import localforage from 'localforage'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$lf = localforage
 
 new Vue({
   router,
