da480c7421946b1c128a73bbbbda94268aaaf265
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-23 21:35:25 +0900

    select 추가

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import localforage from 'localforage'
+import vSelect from 'vue-select'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import 'vue-select/dist/vue-select.css'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$lf = localforage
+
+Vue.component('v-select', vSelect)
 
 new Vue({
   router,
