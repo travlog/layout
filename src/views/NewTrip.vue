694bd1d276317530dd5d9afd6b662e36ee968ffd
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-23 21:35:17 +0900

    폼 수정

--- src/views/NewTrip.vue
+++ src/views/NewTrip.vue
@@ -1,43 +1,31 @@
 <template>
   <div class="new-trip">
-    <h2 class="page-title">New Trip</h2>
     <form @submit.prevent="onSubmit">
       <div class="form-group">
-        <label for="">Name</label>
-        <input type="text" placeholder="지금 정하지 않아도 됩니다.">
-      </div>
-
-      <div class="form-group">
-        <label for="">Start Date</label>
+        <label for="">출발하는 날</label>
         <input type="date">
       </div>
       <div class="form-group">
-        <label for="">출발하는 나라</label>
-        <input type="text">
+        <label for="">시작 도시</label>
+        <v-select :options="cityNames" v-model="departure" />
       </div>
 
       <hr>
 
       <div class="form-group">
-        <label for="">End Date</label>
+        <label for="">돌아오는 날</label>
         <input type="date">
       </div>
 
       <div class="form-group">
-        <label for="">돌아오는 나라</label>
-        <input type="text">
+        <label for="">마지막 도시</label>
+        <v-select :options="cityNames" v-model="arrived" />
       </div>
 
-      <div class="form-group">
-        <label for="same-country-trip">
-          <input id="same-country-trip" name="" type="checkbox">
-          국내 여행입니다.
-        </label>
-
-      </div>
       <div class="form-group">
         <input type="submit">
       </div>
+
       <div class="form-group">
         <button @click="$router.back()">Back</button>
       </div>
@@ -48,6 +36,18 @@
 <script>
 export default {
   name: 'new-trip',
+  data () {
+    return {
+      cities: require('@/locations.json'),
+      departure: '',
+      arrived: ''
+    }
+  },
+  computed: {
+    cityNames () {
+      return this.cities.map(c => c.city)
+    }
+  },
   methods: {
     onSubmit () {
       console.log('hello world')
