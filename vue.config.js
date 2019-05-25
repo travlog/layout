ebcee5182d17b56e1a6bcdfe4741a836cea16ee4
ChangJoo Park <pcjpcj2@gmail.com>
2019-05-25 23:57:58 +0900

    purgecss 추가

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
 module.exports = {
   pwa: {
     themeColor: '#0032aa',
@@ -10,5 +14,28 @@ module.exports = {
 
   css: {
     modules: true
+  },
+
+  configureWebpack: {
+    // Merged into the final Webpack config
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './src/**/*.vue'),
+          path.join(__dirname, './src/**/*.js')
+        ]),
+        extractors: [
+          {
+            extractor: class TailwindExtractor {
+              static extract (content) {
+                return content.match(/[A-z0-9-_:\/]+/g) || []
+              }
+            },
+            extensions: ['html', 'vue', 'js']
+          }
+        ]
+      })
+    ]
   }
 }
